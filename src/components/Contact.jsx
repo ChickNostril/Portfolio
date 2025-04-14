3828b2be468f9fe4e0b71dff4f325611c0637ed0
ChickNostril <chicknostril0626@gmail.com>
2025-04-14 14:58:35 +0900

    [FIX] : HeroAnimation CSS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,40 +10,44 @@ import { profileImage } from "../assets";
 import TreeCanvas from "./canvas/Tree";
 
 const Contact = () => {
-	return (
-		<div className={styles.container}>
-			<motion.div
-				variants={slideIn("left", "tween", 0.2, 1)}
-				className={styles.infoCard}
-			>
-				<p className={styles.title}>Get in touch</p>
-				<h3 className={styles.header}>Contact</h3>
+  return (
+    <div className={styles.container}>
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className={styles.infoCard}
+      >
+        <p className={styles.title}>Get in touch</p>
+        <h3 className={styles.header}>Contact.</h3>
 
-				<div className={styles.infoList}>
-					<div>
-						<img src={profileImage} alt="profile" className={styles.profileImage} />
-					</div>
-					<div>
-						<strong>Email:</strong> strik241@gmail.com
-					</div>
-					<div>
-						<strong>Phone:</strong> +82-10-7127-1089
-					</div>
-					<div>
-						<strong>Address:</strong> 경기도 성남시 수정구 대왕판교로 1210
-					</div>
-				</div>
-			</motion.div>
+        <div className={styles.infoList}>
+          <div>
+            <img
+              src={profileImage}
+              alt="profile"
+              className={styles.profileImage}
+            />
+          </div>
+          <div>
+            <strong>Email:</strong> strik241@gmail.com
+          </div>
+          <div>
+            <strong>Phone:</strong> +82-10-7127-1089
+          </div>
+          <div>
+            <strong>Address:</strong> 경기도 성남시 수정구 대왕판교로 1210
+          </div>
+        </div>
+      </motion.div>
 
-			<motion.div
-				variants={slideIn("right", "tween", 0.2, 1)}
-				className={styles.canvasWrapper}
-			>
-				{/* <EarthCanvas /> */}
-				<TreeCanvas />
-			</motion.div>
-		</div>
-	);
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className={styles.canvasWrapper}
+      >
+        {/* <EarthCanvas /> */}
+        <TreeCanvas />
+      </motion.div>
+    </div>
+  );
 };
 
 export default SectionWrapper(Contact, "contact");
